d7d266784bf439803b34dad7d5959f30280e9c79
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-12-12 09:18:32 -0500

    Update filepath for Slack 3.0.0

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # Add custom CSS tweaks to Slack css to set dark theme.
-FILEPATH=/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/index.js
+FILEPATH=/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/ssb-interop.js
 sudo chmod a+w $FILEPATH
 cat custom.css >> $FILEPATH
 
