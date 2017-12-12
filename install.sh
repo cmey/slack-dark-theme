16a4d5ee58924a83cc0eaeefd25dd8ad1460e491
Christophe Meyer <christophe.meyer.pro@gmail.com>
2017-12-12 09:13:05 -0500

    chmod to make the target file editable

--- install.sh
+++ install.sh
@@ -1,3 +1,5 @@
 # Add custom CSS tweaks to Slack css to set dark theme.
-cat custom.css >> /Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/index.js
+FILEPATH=/Applications/Slack.app/Contents/Resources/app.asar.unpacked/src/static/index.js
+sudo chmod a+w $FILEPATH
+cat custom.css >> $FILEPATH
 
